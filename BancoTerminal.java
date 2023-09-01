665e4af26a1f537df60be5fc2fb8ecb770003a1b
Dan <danielrody@gmail.com>
2023-09-01 18:54:54 -0300

    resolvendo o desafio proposto

---
+++ BancoTerminal.java
@@ -0,0 +1,16 @@
+package edu.dan.ContaBanco;
+
+public class BancoTerminal {
+
+    public static void main(String[] args){
+     double balance = 25.00;
+     double resquestedBalance = 18.00;
+
+     if(balance > resquestedBalance){
+         balance -= resquestedBalance;
+     }else{
+         System.out.println("Saldo insuficiente");
+     }
+        System.out.println(balance);
+    }
+}
